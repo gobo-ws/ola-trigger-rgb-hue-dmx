d9eefcccb2691d07db218f24e9246d3a81119464
Johan Nilsson <jn@gobo.ws>
2018-06-04 14:01:02 +0200

    Update rgbhue.sh

--- rgbhue.sh
+++ rgbhue.sh
@@ -6,10 +6,8 @@
 if [ "$4" = "0" ]
 then
 dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
-red=$( echo ${dmx} | jq -r '.dmx[0]' )
-green=$( echo ${dmx} | jq -r '.dmx[1]' )
-blue=$( echo ${dmx} | jq -r '.dmx[2]' )
-curl -s http://"$3"/rgb-value/${red},${green},${blue} > /dev/null
+rgb=$( echo ${dmx} | jq -r '"\(.dmx[0]),\(.dmx[1]),\(.dmx[2])"' )
+curl -s http://"$3"/rgb-value/${rgb} > /dev/null
 
 elif [ "$4" -gt "0" ]
 then
@@ -17,8 +15,6 @@ dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
 chanone="$4"
 chantwo=$((chanone+1))
 chanthree=$((chantwo+1))
-red=$( echo ${dmx} | jq -r '.dmx['$chanone']' )
-green=$( echo ${dmx} | jq -r '.dmx['$chantwo']' )
-blue=$( echo ${dmx} | jq -r '.dmx['$chanthree']' )
-curl -s http://"$3"/rgb-value/${red},${green},${blue} > /dev/null
+rgb=$( echo ${dmx} | jq -r '"\(.dmx['$chanone']),\(.dmx['$chantwo']),\(.dmx['$chanthree'])"' )
+curl -s http://"$3"/rgb-value/${rgb} > /dev/null
 fi
