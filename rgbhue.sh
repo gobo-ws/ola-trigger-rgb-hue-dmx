a05ea431d5f09df0fe3237da5a1e17ec392f70dc
Johan Nilsson <jn@gobo.ws>
2018-06-26 17:20:42 +0200

    Update rgbhue.sh
    Simplify the way the transitition time is set. Now the script uses the same way as the IKEA Trådfri trigger.

--- rgbhue.sh
+++ rgbhue.sh
@@ -3,44 +3,27 @@
 # rgbhue.sh - OLA Trigger config to control Philips Hue color lights - WIP
 # https://github.com/gobo-ws/ola-trigger-rgb-hue-dmx
 
-if [ "$4" = "0" ] && [ "$5" = "brightness" ]
+if [ "$5" = "brightness" ]
 then
 dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
-transitionTime=$( echo ${dmx} | jq -r '"\(.dmx[5])"' )
-curl -s 'http://'$3'/rgbhue?brightness='$6'&&transitionTime='$transitionTime'' > /dev/null
-elif [ "$4" -gt "0" ] && [ "$5" = "brightness" ]
-then
-dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
-chantranstime=$(($4+5))
-transitionTime=$( echo ${dmx} | jq -r '"\(.dmx['$chantranstime'])"' )
-curl -s 'http://'$3'/rgbhue?brightness='$6'&&transitionTime='$transitionTime'' > /dev/null
+curl -s 'http://'$3'/rgbhue?brightness='$6'&&transitionTime='$7'' > /dev/null
 fi
 if [ "$4" = "0" ] && [ "$5" = "rgb" ]
 then
 dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
 rgb=$( echo ${dmx} | jq -r '"\(.dmx[1]),\(.dmx[2]),\(.dmx[3])"' )
-transitionTime=$( echo ${dmx} | jq -r '"\(.dmx[5])"' )
-curl -s 'http://'$3'/rgbhue-rgb?rgb='$rgb'&&transitionTime='$transitionTime'' > /dev/null
+curl -s 'http://'$3'/rgbhue-rgb?rgb='$rgb'&&transitionTime='$7'' > /dev/null
 elif [ "$4" -gt "0" ] && [ "$5" = "rgb" ]
 then
 dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
 chanred=$(($4+1))
 changreen=$((chanred+1))
 chanblue=$((changreen+1))
-chantranstime=$(($4+5))
 rgb=$( echo ${dmx} | jq -r '"\(.dmx['$chanred']),\(.dmx['$changreen']),\(.dmx['$chanblue'])"' )
-transitionTime=$( echo ${dmx} | jq -r '"\(.dmx['$chantranstime'])"' )
-curl -s 'http://'$3'/rgbhue-rgb?rgb='$rgb'&&transitionTime='$transitionTime'' > /dev/null
+curl -s 'http://'$3'/rgbhue-rgb?rgb='$rgb'&&transitionTime='$7'' > /dev/null
 fi
-if [ "$4" = "0" ] && [ "$5" = "colorTemp" ]
+if [ "$5" = "colorTemp" ]
 then
 dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
-transitionTime=$( echo ${dmx} | jq -r '"\(.dmx[5])"' )
-curl -s 'http://'$3'/rgbhue-ct?colorTemp='$6'&&transitionTime='$transitionTime'' > /dev/null
-elif [ "$4" -gt "0" ] && [ "$5" = "colorTemp" ]
-then
-dmx=$( curl -s http://"$1"/get_dmx?u="$2" )
-chantranstime=$(($4+5))
-transitionTime=$( echo ${dmx} | jq -r '"\(.dmx['$chantranstime'])"' )
-curl -s 'http://'$3'/rgbhue-ct?colorTemp='$6'&&transitionTime='$transitionTime'' > /dev/null
+curl -s 'http://'$3'/rgbhue-ct?colorTemp='$6'&&transitionTime='$7'' > /dev/null
 fi
